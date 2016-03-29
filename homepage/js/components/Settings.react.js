948d7f66f5747bb8a0eb1e80d427aa1b58ba4426
ej <ejay615@gmail.com>
2016-03-29 16:22:28 -0700

    added settings stuff and laughlycss

--- homepage/js/components/Settings.react.js
+++ homepage/js/components/Settings.react.js
@@ -1,20 +1,12 @@
 //Settings Page
 var React = require('react');
 var ReactPropTypes = React.PropTypes;
-var TextInput = require('./TextInput.react');
-//var TextInput = require('./Settings.react');
 
-var labelStyle = {
-  display: 'inline-block'
-}
-var panelStyle = {
-  width: '500px'
-}
+var PageTitle = require('./PageTitle.react');
+var SettingsBox = require('./SettingsBox.react');
 
-//var listStyle ={
 
-   // list-style-type: 'none'
-//}
+
 
 var Settings = React.createClass({
   propTypes: {
@@ -23,29 +15,9 @@ var Settings = React.createClass({
 
   render: function() {
   return (
-  <div >
-           <div className="container">
-        <div className="panel panel-primary" style={panelStyle}>
-          <div className="panel-heading text-center">Account Settings</div>
-          <div className="panel-body text-center">
-            <ul >
-              <li>
-                  <div>
-                    <label style = {labelStyle} className='settingsLabel' HTMLfor='email'>Email</label>
-                    <TextInput type ="text" name='email' id='email' placeholder='email'/>
-                  </div>
-              </li>
-              <li>
-                <h3>
-                  <label className='settingsLabel' HTMLfor='password'>password</label>
-                </h3>
-                <div><TextInput type ="text" name='password' id='password' placeholder='password'/></div>
-              </li>              
-            </ul>
-          </div>
-        </div>
-      </div>
-
+    <div >
+      <PageTitle title="Settings" subtitle="Here is where you can control your settings"/>
+      <SettingsBox section="Account Settings"/>
     </div>
     );
   },
