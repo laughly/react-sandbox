4f8c1db272c226431549c61fc20f440d8a76d39d
ej <ejay615@gmail.com>
2016-03-29 10:48:48 -0700

    added bio

---
+++ homepage/js/components/Biography.react.js
@@ -0,0 +1,24 @@
+//Bio Page
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var TextInput = require('./TextInput.react');
+
+
+var Biography = React.createClass({
+  propTypes: {
+
+  },
+
+  render: function() {
+    return (
+      <div >
+        Biography
+      </div>
+
+    );
+  },
+
+});
+
+module.exports = Biography;
