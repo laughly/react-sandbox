bc2a0559f238e2a90179422186fb24de5bae51d5
ej <ejay615@gmail.com>
2016-04-07 10:08:47 -0700

    updated app.js

--- homepage/js/app.js
+++ homepage/js/app.js
@@ -1,16 +1,16 @@
-/**
- * Copyright (c) 2014, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
 
 var React = require('react');
+
+
 var NavBar = require('./components/NavBar.react');
+var Home = require('./components/Home.react');
+var Biography = require('./components/Biography.react');
+var Catalog = require('./components/Catalog.react');
+var Analytics = require('./components/Analytics.react');
 var Payments = require('./components/Payments.react');
 var Settings = require('./components/Settings.react');
+
+
 const ReactRouter = require('react-router');
 
 const Router = ReactRouter.Router,
@@ -33,6 +33,10 @@ var App = React.createClass({
 const routes = (
 	<Router history={hashHistory}>
 		<Route path="/" component={App}>
+			<Route path="/home" component={Home}/>
+			<Route path="/biography" components={Biography}/>
+			<Route path="/catalog" component={Catalog}/>
+			<Route path="/analytics" components={Analytics}/>
 			<Route path="/payments" component={Payments}/>
 			<Route path="/settings" components={Settings}/>
 		</Route>
